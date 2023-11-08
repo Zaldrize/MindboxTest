5837828950ee73ef8e39878a8886c1735d50ca93
Litvinova Galina <litvinovaga@inbox.ru>
2023-11-08 23:35:59 +0300

    Base classes and common interface

---
+++ Shapes/Shapes/Circle.cs
@@ -0,0 +1,20 @@
+﻿namespace Shapes
+{
+    public class Circle: IShape
+    {
+        private double _radius;
+        public Circle(double radius)
+        {
+            if (radius < 0)
+            {
+                throw new Exception("That is not valid radius");
+            }
+            _radius = radius;
+
+        }
+        public double GetArea()
+        {
+           return Math.PI * _radius * _radius;
+        }
+    }
+}
---
+++ Shapes/Shapes/Triangle.cs
@@ -0,0 +1,26 @@
+﻿namespace Shapes
+{
+    internal class Triangle : IShape
+    {
+        private double _a, _b, _c;
+        public Triangle(double a, double b, double c)
+        {
+            _a = a;
+            _b = b;
+            _c = c;
+            if (!IsValid())
+            {
+                throw new Exception("That is not a valid triangle");
+            }
+        }
+        private bool IsValid()
+        {
+            return _a < _b + _c && _b < _a + _c && _c < _a + _b;
+        }
+        public double GetArea()
+        {
+            var p = (_a + _b + _c) / 2;
+            return Math.Sqrt(p*(p - _a)*(p - _b)*(p - _c));
+        }
+    }
+}
